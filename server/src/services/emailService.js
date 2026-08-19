03d8d8c4358795c906221683cb00e6001551e932
Mikiyas Nigatu <mikermount19@yahoo.com>
2026-08-19 15:56:40 -0300

    refactor(email): disable automatic SSL selection in transporter

--- server/src/services/emailService.js
+++ server/src/services/emailService.js
@@ -5,7 +5,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
-  secure: Number(process.env.EMAIL_PORT) === 465,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
